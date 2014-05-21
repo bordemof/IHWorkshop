ebcf6579cbf952e5efacf3f9e072b1623ecb776d
Imanol Yáñez <lonami80@gmail.com>
2014-05-21 22:50:08 +0200

    Added FizzBuzz kata template

---
+++ 4/js/app.js
@@ -0,0 +1,7 @@
+fizzBuzz = function(){
+  number = document.getElementById("input-fizzbuzz").value;
+  console.log(number);
+  //Aqui vuestra logica
+
+  document.getElementById("result").innerHTML = "Aqui el resultado!";
+}
